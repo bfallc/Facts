514c7924c982200d9a093090e5a0c486c9dee473
bfallc <bfallc@artic.edu>
2020-01-31 09:37:22 -0600

    Update try.py

--- docs/try.py
+++ docs/try.py
@@ -5,7 +5,7 @@
 import cgitb
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
+print("Content-Type: text/plain;charset=utf-8")
+print('help')
 
-print "Hello World!"
+print("hello world")
