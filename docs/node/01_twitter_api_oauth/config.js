322ec0696f0e3f1960af179732f65675f333de76
bfallc <60400018+bfallc@users.noreply.github.com>
2020-02-01 11:11:43 -0600

    Update and rename config-empty.js to config.js

---
+++ docs/node/01_twitter_api_oauth/config.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+    consumer_key:         'SoHbJoMHWFIJLDwETFEnZ19oG',
+    consumer_secret:      'ZQOJSEtFmXrYQSWvqdo8w7bHHchZMN84pCybo8UAtA9ahN6Ngg',
+    access_token:         '1197684969411403782-AiYbaHtpx4u6bq1lzWaRPUtAeTcd1e',
+    access_token_secret:  'UO1J7bdmVdvdu1aWWBEFxg1S0s1CzpwcT9ZuOJ4GujPEq',
+}
