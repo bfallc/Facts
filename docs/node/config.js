aac2a52bcfb96d81a15ff12ce96fcf85bc24bfea
bfallc <60400018+bfallc@users.noreply.github.com>
2020-02-01 23:01:01 -0600

    Delete config.js

--- docs/node/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'SoHbJoMHWFIJLDwETFEnZ19oG',
-    consumer_secret:      'ZQOJSEtFmXrYQSWvqdo8w7bHHchZMN84pCybo8UAtA9ahN6Ngg',
-    access_token:         '1197684969411403782-AiYbaHtpx4u6bq1lzWaRPUtAeTcd1e',
-    access_token_secret:  'UO1J7bdmVdvdu1aWWBEFxg1S0s1CzpwcT9ZuOJ4GujPEq',
-}
